4bfd5306d0e686ea843b47c0b303b2d42dc84bfa
Shaheed Shamsi <shamsishaheed@gmail.com>
2018-06-16 12:50:44 -0500

    added more stuff

--- src/NewEntry.js
+++ src/NewEntry.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 export default class Log extends React.Component {
   constructor() {
     super();
+    this.state = {
+      newDate: '',
+      newEntry: ''
+    }
   }
 
+  handleDatePress = (event) => {
+    this.setState({
+      newDate: event
+    })
+  }
 
-  handleClick(event) {
+  handleEntryPress = (event) => {
+    this.setState({
+      newEntry: event
+    })
+  }
+
+  handleClick = (event) => {
     
   }
   render() {
     return (
       <View style={styles.ViewWrap}>
-        <Text>add a new entry here!</Text>
+        <View style={styles.dateInputWrap}>
+          <TextInput
+          style={styles.dateInput}
+          value={this.state.newDate}
+          placeholder='Add a date!'
+          onChangeText={this.handleDatePress}
+          />
+        </View>
+        <View style={styles.inputWrap}>
+          
+          <TextInput
+          multiline={true}
+          style={styles.input}
+          onChangeText={this.handleEntryPress}
+          value={this.state.newEntry}
+          placeholder='Add a new entry here!'
+          />
+        </View>
+        <Text>{this.state.newEntry}</Text>
         <Button
         title='Add'
         style={styles.newEntryBtn}
@@ -28,6 +61,43 @@ export default class Log extends React.Component {
 const styles = StyleSheet.create({
   ViewWrap: {
     flex: 1,
+    backgroundColor: '#bed4f7'
+  },
+  dateInputWrap: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 30,
+    marginLeft: 30,
+    marginRight: 147,
+    height: 35,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    // color: 'red',
+    borderWidth: 1
+  },
+  dateInput: {
+    
+  },
+  inputWrap: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 5,
+    marginLeft: 30,
+    marginRight: 30,
+    height: 200,
+    borderColor: 'black',
+    borderRadius: 5,
+    backgroundColor: 'white',
+    // color: 'red',
+    borderWidth: 1
+  },
+  input: {
+    // borderColor: 'black',
   },
   newEntryBtn: {
     alignItems: 'center',
